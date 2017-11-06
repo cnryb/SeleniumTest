861734d80dcf4b1ac7f1818d0d8c5078bef8526e
任跃兵 <cnryb@live.cn>
2017-11-06 13:43:26 +0800

    测试通过

---
+++ SeleniumTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://www.baidu.com");
+            driver.FindElement(By.Id("kw")).SendKeys("Selenium");
+            driver.FindElement(By.Id("su")).Click();
+
+            driver.Quit();
+
+        }
+    }
+}
